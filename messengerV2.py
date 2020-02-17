9d1f5509c888856926c259745a6e2a4b7c24a1a5
Sulfur0 <aevega1991@gmail.com>
2020-02-17 10:52:57 -0300

    Notificaciones en OneSignal, Persistencia y consultas a bases de datos completas y funcionales

--- messengerV2.py
+++ messengerV2.py
@@ -6,7 +6,11 @@ import requests
 import io
 from datetime import datetime
 
-def mysqlConnect():
+# Distintas consultas para distintas audiencias
+consultasPersonales = "a.id_notif_cuerpo = b.id_notif_cuerpo and a.fk_cabecera=1"
+consultasGenerales = "a.id_notif_cuerpo = b.id_notif_cuerpo and a.fk_cabecera=2"
+
+def mysqlConnect(consulta):
 	# Abre la conexión a base de datos
 	db = pymysql.connect("192.168.50.98","appusr","1xz9tv48","intranet" )
 
@@ -14,14 +18,12 @@ def mysqlConnect():
 	cursor = db.cursor()
 
 	# ejecuta un query SQL usando execute()
-	cursor.execute("select a.user,a.asunto,a.mensaje from app_notif_cuerpo as a join ( select `user`, min(`id_notif_cuerpo`) as `id_notif_cuerpo` from `app_notif_cuerpo` group by `user` ) as b on a.user = b.user where a.id_notif_cuerpo = b.id_notif_cuerpo and a.fk_cabecera=1")
-	# Obtiene una sola fila usando fetchone()
+	cursor.execute("select a.user,a.asunto,a.mensaje from app_notif_cuerpo as a join ( select `user`, min(`id_notif_cuerpo`) as `id_notif_cuerpo` from `app_notif_cuerpo` where `fk_cabecera` = 2 group by `user` ) as b on a.user = b.user where "+consulta)
+
 	data = cursor.fetchall()
 	db.close()
 
 	return data
-
-data = mysqlConnect()
 
 # Diccionario que ordena apropiadamente las notificaciones segun usuarios
 # para evitar enviar múltiples mensajes con el mismo contenido a distintos usuarios,
@@ -33,7 +35,7 @@ data = mysqlConnect()
 def crearNotif(notificaciones,elem):
 	notif = {}
 	usuarios = []
-	usuarios.append(elem[0])
+	usuarios.append(str(elem[0]))
 	notif["user"] = usuarios
 	notif["title"] = elem[1]
 	notif["body"] = elem[2]
@@ -41,7 +43,15 @@ def crearNotif(notificaciones,elem):
 
 
 #@function: toda la lógica interna para crear la notificación per se en formato json
-def generarJson():
+#@param criterio: criterio bajo el cual se buscará la información en base de datos
+def generarJson(criterio):
+	data = []
+	if criterio == "personal":
+		data = mysqlConnect(consultasPersonales)
+	
+	elif criterio == "general":
+		data = mysqlConnect(consultasGenerales)
+	
 	# función para obtener 
 	notificaciones = []
 	for elem in data:
@@ -50,7 +60,7 @@ def generarJson():
 		for notif in notificaciones:						
 			if elem[1] == notif["title"] and elem[2] == notif["body"] and len(notif["user"]) < 99:
 				if elem[0] not in notif["user"]:
-					notif["user"].append(elem[0])
+					notif["user"].append(str(elem[0]))
 					match = 1			
 					break
 			if len(notif["user"]) >= 99:
@@ -79,7 +89,12 @@ def generarJson():
 		arr_notification_target["type"] = arr_notif_type
 		arr_notification_target["user_ids"] = arr_notif_user_ids
 		
-		notif["notification_target"] = arr_notification_target
+		if criterio == "personal":
+			notif["notification_target"] = arr_notification_target
+	
+		elif criterio == "general":
+			notif["notification_target"] = None
+		
 		json.append(notif)
 
 	#array json contiene notificaciones pero deben enviarse individualmente
@@ -97,15 +112,19 @@ def enviar(data):
 
 def main():
 	minutos = 60
+	critarr = ["general"]
 	while 1:
-		arr = generarJson()
-
-		# envía cada objeto notificación guardado en el array arr
-		for data in arr:
-			print("Enviando payload")
-			enviar(data)
-			time.sleep(10)
-
+		arr = []
+		for crit in critarr:			
+			arr = generarJson(crit)
+		
+			# envía cada objeto notificación guardado en el array arr
+			for data in arr:
+				print("Enviando payload "+crit)
+				enviar(data)
+				#print(data)
+				time.sleep(10)
+				
 		print("Se buscará nuevas notificaciones en "+str(minutos)+" minutos")
 		time.sleep(minutos*60)
 
---
+++ messengerV3.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+import time
+import json
+import pymysql
+import requests
+import io
+from datetime import datetime
+
+# OBTENER LA CABECERA, LUEGO OBTENER TODOS LOS USERIDS QUE LE PEGUEN A ESA CABECERA Y ARMAR LA NOTIFICACION
+
+def obtenerCabeceras():
+	db = pymysql.connect("192.168.50.98","appusr","1xz9tv48","intranet" )
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM `app_notif_cabecera`")
+	data = cursor.fetchall()
+	db.close()
+	return data
+
+def mysqlConnect(cabecera):
+	db = pymysql.connect("192.168.50.98","appusr","1xz9tv48","intranet" )
+	cursor = db.cursor()
+	cursor.execute("SELECT DISTINCT deviceid FROM `app_user_devices` as d join app_notif_cuerpo as c on d.user_id = c.fk_user_objetivo where c.estado = 0 and fk_cabecera = "+str(cabecera[0]))
+
+	data = cursor.fetchall()
+	db.close()
+	notifs = {}
+	notifs["cabecera"]=cabecera
+	notifs["objetivos"]=data
+
+	return notifs
+
+#@function: toda la lógica interna para crear la notificación per se en formato json
+#@param criterio: criterio bajo el cual se buscará la información en base de datos
+def generarJson(consulta):
+	#print (consulta)
+	resultado = {}
+	resultado["app_id"] = "5bd931bc-a426-44ec-85a5-bfd47a771213"
+
+	players = []
+	for target in consulta['objetivos']:
+		players.append(target[0])
+	resultado["include_player_ids"] = players
+
+	titulo = {}
+	titulo["en"] = consulta["cabecera"][1]
+	resultado["headings"] = titulo
+
+	contenido = {}
+	text = {}
+	text["en"] = consulta["cabecera"][2]
+	resultado["contents"] = text
+
+	return resultado
+
+def enviar(data):
+
+	data_dumpeada = json.dumps(data)
+	url = 'https://onesignal.com/api/v1/notifications'
+	cabecera = {}
+	cabecera["Authorization"] = 'YTJiMWM1MmQtNWQxZi00OGVhLWI5MjYtYjI5MWY4N2ViZDAw'
+	cabecera["Content-Type"] = 'application/json'
+	print(data_dumpeada)
+	print("Enviado push con resultado:")
+	x = requests.post(url=url, data = data_dumpeada, headers = cabecera);	
+	print(x)
+	print()
+
+def generarNotificaciones():
+
+	
+
+def main():
+	minutos = 60
+	
+	while 1:
+
+		generarNotificaciones()
+		cabeceras = obtenerCabeceras()
+		for cabecera in cabeceras:
+			consulta = mysqlConnect(cabecera)
+			json = generarJson(consulta)
+			#print(json)
+			enviar(json)
+			print("Esperando 30 segundos para la siguiente notificación...")
+			time.sleep(30)
+		
+		print("Se buscarán nuevas notificaciones en "+str(minutos)+" minutos")
+		time.sleep(minutos*60)
+
+main()
