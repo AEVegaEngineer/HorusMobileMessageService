8d4243b07813d5a2b270b06e08625cfaf09824eb
Sulfur0 <aevega1991@gmail.com>
2020-03-13 12:02:28 -0300

    Se ha mejorado el servicio para revisar cuando ya se haya enviado una notificacion y no reenviarla hasta dentro de 1 día despues

--- messengerV3.py
+++ messengerV3.py
@@ -7,46 +7,48 @@ import io
 from datetime import datetime
 
 # OBTENER LA CABECERA, LUEGO OBTENER TODOS LOS USERIDS QUE LE PEGUEN A ESA CABECERA Y ARMAR LA NOTIFICACION
-
-def obtenerCabeceras():
+def mysqlConnect(consulta):
 	db = pymysql.connect("192.168.50.81","remoto","1xz9tv48","intranet" )
 	cursor = db.cursor()
-	cursor.execute("SELECT * FROM `app_notif_cabecera`")
+	cursor.execute(consulta)
 	data = cursor.fetchall()
 	db.close()
 	return data
 
-def mysqlConnect(cabecera):
+def obtenerCabeceras():
+	# SELECCIONA UNA SOLA CABECERA, LUEGO SE SELECCIONAN VARIOS USUARIOS PARA ESA CABECERA
+	consulta = "SELECT DISTINCT cab.* FROM `app_notif_cabecera` as cab join app_notif_cuerpo as cue on cab.id_cabecera = cue.fk_cabecera where cue.estado = 0"
+	data = mysqlConnect(consulta)	
+	return data
+
+def getCabeceras(cabecera):
 	
-	db = pymysql.connect("192.168.50.81","remoto","1xz9tv48","intranet" )
-	cursor = db.cursor()
 	#consulta = "SELECT DISTINCT deviceid FROM `app_user_devices` as d join app_notif_cuerpo as c on d.user_id = c.fk_user_objetivo where c.estado = 0 and fk_cabecera = "+str(cabecera[0])
 
-	consulta = "SELECT deviceid FROM `app_user_devices` as d join app_notif_cuerpo as c on d.user_id = c.fk_user_objetivo where c.estado = 0 and fk_cabecera = "+str(cabecera[0])
+	consulta = "SELECT d.deviceid FROM `app_user_devices` as d join app_notif_cuerpo as c on d.user_id = c.fk_user_objetivo where c.estado = 0 and fk_cabecera = "+str(cabecera[0])+" and (c.f_hs_ultimo_envio + INTERVAL 1 DAY <= now() or c.f_hs_ultimo_envio is null or c.f_hs_ultimo_envio = '0000-00-00 00:00:00')"
+	data = mysqlConnect(consulta)	
 
+	actualizacion = "update app_notif_cuerpo set f_hs_ultimo_envio = now() where estado = 0 and fk_cabecera = "+str(cabecera[0])+" and (f_hs_ultimo_envio + INTERVAL 1 DAY <= now() or f_hs_ultimo_envio is null or c.f_hs_ultimo_envio = '0000-00-00 00:00:00')"
+	mysqlConnect(actualizacion)
 
-	#print ("************")
-	#print (consulta)
-	#print ("************")
-	cursor.execute(consulta)
-
-	data = cursor.fetchall()
-	db.close()
 	notifs = {}
 	if data == ():
 		return "empty_notif"
 	notifs["cabecera"]=cabecera
-	notifs["objetivos"]=data
-
+	
+	objetivos = []
+	for target in data:
+		objetivos.append(target)
+	notifs["objetivos"] = objetivos
+	
 	return notifs
 
 #@function: toda la lógica interna para crear la notificación per se en formato json
 #@param criterio: criterio bajo el cual se buscará la información en base de datos
 def generarJson(consulta):
-	#print (consulta) 
 	resultado = {}
 	resultado["app_id"] = "5bd931bc-a426-44ec-85a5-bfd47a771213"
-
+	#print(consulta['objetivos'])
 	players = []
 	for target in consulta['objetivos']:
 		players.append(target[0])
@@ -60,7 +62,8 @@ def generarJson(consulta):
 	text = {}
 	text["en"] = consulta["cabecera"][2]
 	resultado["contents"] = text
-
+	#print ("**********resultado***********")
+	#print (resultado)
 	return resultado
 
 def enviar(data):
@@ -74,12 +77,14 @@ def enviar(data):
 	print("Enviado push con resultado:")
 	x = requests.post(url=url, data = data_dumpeada, headers = cabecera);	
 	print(x)
+	# MARCA LA HORA DEL ENVÍO DE LA NOTIFICACIÓN
 	print()
 
 
 def main():
+
 	print ("Servicio de Mensajería Iniciado")
-	minutos = 1
+	minutos = 2
 	tiempoEntreNotificaciones = 5
 	
 	while 1:
@@ -87,7 +92,9 @@ def main():
 		tiempo = time.strftime("%d/%m/%Y %H:%M:%S")
 		cabeceras = obtenerCabeceras()
 		for cabecera in cabeceras:
-			consulta = mysqlConnect(cabecera)
+			#print ("Obteniendo todas las notificaciones de "+cabecera[1])
+			consulta = getCabeceras(cabecera)
+			#print (consulta)
 			if consulta == "empty_notif":
 				continue
 			json = generarJson(consulta)
@@ -101,5 +108,5 @@ def main():
 		
 		print(tiempo+" Se buscarán nuevas notificaciones en "+str(minutos)+" minutos")
 		time.sleep(minutos*60)
-
+	
 main()
